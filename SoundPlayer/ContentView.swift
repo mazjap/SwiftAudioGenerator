97ddec42800cd1030471d63d0ac5e9682ffc734c
Jordan Christensen <jordan.c4922@gmail.com>
2024-04-30 13:14:04 -0600

    Initial Commit

---
+++ SoundPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundPlayer
+//
+//  Created by Jordan Christensen on 4/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundPlayer/SoundPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundPlayerApp.swift
+//  SoundPlayer
+//
+//  Created by Jordan Christensen on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
